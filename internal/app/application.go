efa20dd9781ea7c61b070c2447a101c00aa3e0cc
devpool <dungnguyen@co-pro.vn>
2026-03-03 21:51:57 +0700

    set static to storage

--- internal/app/application.go
+++ internal/app/application.go
@@ -18,18 +18,22 @@ func NewApplication() *Application {
 	db, err := gorm.Open(mysql.Open("root:@tcp(127.0.0.1:3306)/family?charset=utf8mb4&parseTime=True&loc=Local"), &gorm.Config{})
 
 	if err != nil {
-		panic("Failed to connect database")
+		panic("Failed to connect database, check username, password, host:port and database exist")
 	}
 
-	db.AutoMigrate(&models.User{})
+	if err = db.AutoMigrate(&models.User{}); err != nil {
+		panic("Failed to migrate database")
+	}
 
 	app := gin.Default()
 
-	modules := []modules.Module{
+	app.Static("/storage", "./storage")
+
+	allModules := []modules.Module{
 		modules.NewUserModule(db),
 	}
 
-	routes.RegisterRoutes(app, GetModuleRoutes(modules)...)
+	routes.RegisterRoutes(app, GetModuleRoutes(allModules)...)
 
 	return &Application{
 		App: app,
